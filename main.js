fd425ff44d88b95139935428e7cd9242fe7b8f33
harsha383 <82648520+harsha383@users.noreply.github.com>
2022-01-23 17:12:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(600,600);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('img_filter_web_app.png')
+}
